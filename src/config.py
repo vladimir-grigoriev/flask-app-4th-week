a4af0def7bde3db5916a69e32a91ef585e3cce2f
vladimir_grigoriev <vladimir_grigoriev@mail.ru>
2021-02-22 23:42:12 +0300

    changed env file

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ import os
 
 class Configuration(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
